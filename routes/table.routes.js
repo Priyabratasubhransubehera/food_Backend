11d2f1f3f76553ee5ff5cc3dae7367a87eb01df7
Priyabratasubhransubehera <priyabrata.subhransu@github.com>
2026-07-01 11:31:28 +0530

    first commit

---
+++ routes/table.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { CreateTableBook, deleteBooking, getAllBookings } from "../controllers/table.controller.js";
+
+
+const TableRoutes = express.Router();
+
+TableRoutes.post("/create", CreateTableBook);
+TableRoutes.get("/get-all", getAllBookings);
+TableRoutes.delete("/delete/:id", deleteBooking);
+
+
+
+
+
+export default TableRoutes;
